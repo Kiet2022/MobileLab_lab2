bb0e3cbfd11aceef7cfcfa8162886d1d9bfb48a5
Kiet2022 <108647997+Kiet2022@users.noreply.github.com>
2023-10-25 09:43:31 +0700

    done q2

---
+++ Question2/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginScreen from './Login/Login';
+import { View,Text } from 'react-native';
+
+
+export default App =() =>{
+  return (
+    //<View><Text>123</Text></View>
+    
+    <LoginScreen/>
+  );
+};
